f4eb47d04b6f541fe88eca2d2313fda16fe72db6
ulysses <faustolopez110@gmail.com>
2019-10-16 23:45:36 -0400

    Updates

---
+++ precache-manifest.e501564cebce83b18786332291380506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff58cce8574b77871c25f51411cc5b5",
+    "url": "/nyc-link/index.html"
+  },
+  {
+    "revision": "b8e5ff80329bdf833fc4",
+    "url": "/nyc-link/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "730a04a368b51aaea3f5",
+    "url": "/nyc-link/static/css/main.53984709.chunk.css"
+  },
+  {
+    "revision": "b8e5ff80329bdf833fc4",
+    "url": "/nyc-link/static/js/2.39afa083.chunk.js"
+  },
+  {
+    "revision": "730a04a368b51aaea3f5",
+    "url": "/nyc-link/static/js/main.afa95ba0.chunk.js"
+  },
+  {
+    "revision": "6a9ec066a62946af5616",
+    "url": "/nyc-link/static/js/runtime-main.873cd7a2.js"
+  }
+]);
