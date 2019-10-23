cf2310f2da036bf6ce5995ae0238fec7eb61856e
ulysses <faustolopez110@gmail.com>
2019-10-23 01:28:51 -0400

    Updates

---
+++ precache-manifest.848c47e3fb8a2dc1cb52fa8bc4307638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a045fa991a2f9f80f6e3626b09f8885b",
+    "url": "/nyc-link/index.html"
+  },
+  {
+    "revision": "0409d035d0ffd51f7557",
+    "url": "/nyc-link/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e8b853ed30cf6ce461b5",
+    "url": "/nyc-link/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0409d035d0ffd51f7557",
+    "url": "/nyc-link/static/js/2.f1ff7894.chunk.js"
+  },
+  {
+    "revision": "e8b853ed30cf6ce461b5",
+    "url": "/nyc-link/static/js/main.1669d217.chunk.js"
+  },
+  {
+    "revision": "6a9ec066a62946af5616",
+    "url": "/nyc-link/static/js/runtime-main.873cd7a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nyc-link/precache-manifest.4ee3c8f9a092a9d0b1d2a739e5a87abf.js"
+  "/nyc-link/precache-manifest.848c47e3fb8a2dc1cb52fa8bc4307638.js"
 );
 
 self.addEventListener('message', (event) => {
